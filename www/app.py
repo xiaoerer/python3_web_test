b762d0c5800b68b45fcfaa503729ca44a9e631c6
xiezhengjun <527468075@qq.com>
2017-12-28 21:49:56 +0800

    com push

--- www/app.py
+++ www/app.py
@@ -13,6 +13,8 @@ from config import configs
 import orm
 from coroweb import add_route, add_routes, add_static
 
+from handlers import cookie2user,COOKIE_NAME
+
 def init_jinja2(app, **kw):
     logging.info('init jinja2...')
     options = dict(
--- www/handlers.py
+++ www/handlers.py
@@ -5,19 +5,72 @@ __author__='xiaoer'
 
 import re, time, json, logging, hashlib, base64, asyncio
 
-from coroweb import get, post
-
-from models import User, Comment, Blog, next_id
-
-from apis import APIValueError, APIResourceNotFoundError
-
-from config import configs
+import markdown2
 
 from aiohttp import web
 
-import markdown2
+from coroweb import get, post
+from apis import Page, APIValueError, APIResourceNotFoundError
+
+from models import User, Comment, Blog, next_id
+from config import configs
+
+COOKIE_NAME='xiaoersession'
+_COOKIE_KEY='configs.session.secret'
 
 
+def check_admin(request):
+	if request.__user__ is None or not request.__user__.admin:
+		raise APIPermissionError()
+
+def get_page_index(page_str):
+	p=1
+	try:
+		p=int(page_str)
+	except ValueError as e:
+		raise e
+		pass
+	if p<1:
+		p=1
+	return p
+
+def user2cookie(user,max_age):
+	expires=str(int(time.time()+max_age))
+	s='%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
+	L=[user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+	return '-'.join(L)
+
+
+def text2html(text):
+    lines = map(lambda s: '<p>%s</p>' % s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;'), filter(lambda s: s.strip() != '', text.split('\n')))
+	return ''.join(lines)
+
+@asyncio.coroutine
+def cookie2user(cookie_str):
+    '''
+    Parse cookie and load user if cookie is valid.
+    '''
+    if not cookie_str:
+        return None
+    try:
+        L = cookie_str.split('-')
+        if len(L) != 3:
+            return None
+        uid, expires, sha1 = L
+        if int(expires) < time.time():
+            return None
+        user = yield from User.find(uid)
+        if user is None:
+            return None
+        s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
+        if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
+            logging.info('invalid sha1')
+            return None
+        user.passwd = '******'
+        return user
+    except Exception as e:
+        logging.exception(e)
+		return None
 
 """
 @get('/')
